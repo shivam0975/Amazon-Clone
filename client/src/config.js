7f954b538fbd36c2f7dce852625f97cf922ae75e
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-08-11 21:29:42 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const API_BASE =
-  process.env.REACT_APP_API_URL && process.env.REACT_APP_API_URL.length
-    ? process.env.REACT_APP_API_URL
+  process.env.NODE_ENV === "production"
+    ? "https://amazon-clone-backend-p4ol.onrender.com"
     : "http://localhost:8000";
 
 export default API_BASE;
